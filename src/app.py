21a9f05161fecc1937ec5cb2f3bf9d96f9d3ca3a
Tadeu Madureira <tadeucbmad@gmail.com>
2025-06-25 22:52:47 -0400

    adding code

--- src/app.py
+++ src/app.py
@@ -27,7 +27,7 @@ def read_users():
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
-           status_code = HTTPStatus.NOT_FOUND, detail='User not found'
+           status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
     user_with_id = UserDB(**user.model_dump(), id=user_id)
@@ -40,7 +40,7 @@ def update_user(user_id: int, user: UserSchema):
 def delete_user(user_id: int):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, message='User not found'
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
     del database[user_id - 1]
